d1ecbb2259b9c93c79ba57e33c3a164c50db8c77
InGraw <166131364+InGraw-Co@users.noreply.github.com>
2025-12-28 09:59:28 +0100

    Initialize frames module with frame imports

---
+++ frames/_init_.py
@@ -0,0 +1,5 @@
+from .welcome_frame import WelcomeFrame
+from .config_frame import ConfigFrame
+from .intermediate_frame import IntermediateFrame
+from .editor_frame import EditorFrame
+from .finish_frame import FinishFrame
